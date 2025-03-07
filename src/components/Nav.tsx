7772bd3ff86a6baa908b551734f8267f1fa6db56
Lukas deConantsesznak <lukas@lunoho.company>
2025-03-06 19:34:22 -0700

    feat: security page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,7 +13,7 @@ export const Nav = () => {
           width={48}
           height={48}
         />
-        <h1 className="text-2xl font-bold">SteakStache</h1>
+        <h1 className="text-2xl font-bold text-neutral-100">SteakStache</h1>
       </div>
       <WalletMultiButton className="bg-[#090C08] hover:bg-[#18AB7C] cursor-pointer text-neutral-100 px-4 py-2 rounded-md transition-colors duration-300 ease-in-out" />
     </header>
