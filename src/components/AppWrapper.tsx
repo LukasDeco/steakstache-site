4f24d7265b89ab3074b4aa829be4097aed922859
Lukas deConantsesznak <lukas@lunoho.company>
2025-03-12 22:32:05 -0600

    fix: mainnet rpc

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -6,23 +6,20 @@ import {
   useWallet,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 
+export const MAINNET_RPC_URL = process.env.NEXT_PUBLIC_MAINNET_RPC_URL;
+
 export const AppWrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
   const { wallet } = useWallet();
 
-  console.log(wallet);
-
   // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
     () => [
@@ -41,11 +38,13 @@ export const AppWrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
       new UnsafeBurnerWalletAdapter(),
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [network]
+    [MAINNET_RPC_URL]
   );
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
+    <ConnectionProvider
+      endpoint={MAINNET_RPC_URL ?? WalletAdapterNetwork.Mainnet}
+    >
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
