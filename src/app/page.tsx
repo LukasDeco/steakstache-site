b1b2d98ec4c58ee1c2d642c29c6aebbec52022c0
Lukas deConantsesznak <lukas@lunoho.company>
2025-03-13 22:40:43 -0600

    fix: rm crazy section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { AppWrapper } from "@/components/AppWrapper";
 import { Hero } from "@/components/Hero";
 import { Nav } from "@/components/Nav";
-import StatsSection from "@/components/StatsSection";
+// import StatsSection from "@/components/StatsSection";
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 pt-4 row-start-2 text-neutral-100 justify-between items-center sm:items-start bg-[radial-gradient(100%_100%_at_50%_80%,#FFDADE_0%,#18AB7C_40%,#090C08_70%)] h-screen w-full min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
@@ -12,7 +12,7 @@ export default function Home() {
             <Hero />
           </div>
         </div>
-        <StatsSection />
+        {/* <StatsSection /> */}
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center w-full">
           SteakStache © 2025
         </footer>
