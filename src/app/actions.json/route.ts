0aed631f49f9df02aefe3f1b7c44129daf9b39b9
Lukas deConantsesznak <lukas@lunoho.company>
2025-05-02 09:27:28 -0600

    revert: revert actions www paths

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -6,12 +6,12 @@ export const GET = async () => {
       // map all root level routes to an action
       {
         pathPattern: "/*",
-        apiPath: "www.steakstache.com/api/*",
+        apiPath: "/api/*",
       },
       // idempotent rule as the fallback
       {
         pathPattern: "/api/**",
-        apiPath: "www.steakstache.com/api/**",
+        apiPath: "/api/**",
       },
     ],
   };
